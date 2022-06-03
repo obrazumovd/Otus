ac12d4e11c81bce5d48de6416d5c1c65459378c7
Dmitriy Obrazumov <obrazumovd@mail.ru>
2022-06-03 18:05:18 +0300

    Initial Commit

---
+++ Otus/OtusApp.swift
@@ -0,0 +1,17 @@
+//
+//  OtusApp.swift
+//  Otus
+//
+//  Created by Dmitriy Obrazumov on 03/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct OtusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
